b556f510658f69305498af4447cdcfcb52850b02
Loanbrwsk <Loan.Borowski@protonmail.com>
2026-02-18 12:09:05 +0100

    rm session_start()

--- views/create-account.php
+++ views/create-account.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $_SESSION['pseudo'] = "Invite";
 ?>
 <!DOCTYPE html>
--- views/login.php
+++ views/login.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 $_SESSION['pseudo'] = "Invité";
 $_SESSION['progress'] = 0;
 ?>
