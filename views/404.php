d6f664d2805d801efafe6517b78d96302750b170
Loanbrwsk <Loan.Borowski@protonmail.com>
2026-02-25 16:48:26 +0100

    update css

--- views/404.php
+++ views/404.php
@@ -6,30 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="assets/img/neonquests_logo.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>NeonQuests</title>
 </head>
 
 <body>
-
-<!-- <style>
-    .page-not-found-container{
-    position: fixed;
-    top: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 100vh;
-    }
-
-    .page-not-found{
-        font-size: 65px;
-    }
-
-</style> -->
-
     <div class="image-container">
         <img src="assets/img/neonquests_full_logo.svg" alt="neonquests-logo">
     </div>
