e87df0faf953eada3b6acb6c05b1b55932fdd5bb
Noxie07 <matthias.bourdon51@gmail.com>
2026-03-02 15:58:15 +0100

    position + pointillé + bloc thème

--- views/home.php
+++ views/home.php
@@ -23,6 +23,7 @@
             border-radius: 50%;
             border: none;
             position: relative;
+            top : 80px;
             background: inherit;
             transition: 0.1s;
             background: linear-gradient(90deg,
@@ -31,6 +32,28 @@
                     rgba(210, 0, 247, 1) 100%);
             background-attachment: fixed;
             overflow: hidden;
+            z-index: 99;
+        }
+        .boutonlevel1 {
+            width: 155px;
+            height: 150px;
+            font-weight: bolder;
+            cursor: pointer;
+            color: #ffffff;
+            font-size: 17px;
+            border-radius: 50%;
+            border: none;
+            position: relative;
+            top: -80px;
+            background: inherit;
+            transition: 0.1s;
+            background: linear-gradient(90deg,
+                    rgba(0, 154, 214, 1) 0%,
+                    rgba(39, 64, 163, 1) 50%,
+                    rgba(210, 0, 247, 1) 100%);
+            background-attachment: fixed;
+            overflow: hidden;
+            z-index: 99;
         }
         .boutonlevel::after {
             content: '';
@@ -39,11 +62,22 @@
             filter: blur(15px);
             z-index: -1;
             position: absolute;
-            left: 0;
-            top: 0;
+            
+        }
+        .boutonlevel1::after {
+            content: '';
+            width: 100%;
+            height: 100%;
+            filter: blur(15px);
+            z-index: -1;
+            position: relative;
+            
         }
 
         .boutonlevel:active {
+            transform: scale(0.95);
+        }
+        .boutonlevel1:active {
             transform: scale(0.95);
         }
 
@@ -54,8 +88,10 @@
             margin-left: 50px;
             margin-right: 50px;
             border-radius: 20px;
+            gap: 80px;
 
         }
+        
     </style>
     
     <div class="top-bar">
@@ -76,14 +112,65 @@
     </div>
 
     <div class="box_btn">
+        <div class="onglet-titre">thème 1 : je mange des pâtes</div>
         <button class="boutonlevel">
             LEVEL 1
         </button>
+        <svg class="line">
+            <line x1="130" y1="110" x2="240" y2="40" />
+            </svg>
+        <button class="boutonlevel1">
+            LEVEL 2
+        </button>
+            <svg class="line">
+            <line x1="480" y1="110" x2="240" y2="-60" />
+            </svg>
+        <button class="boutonlevel">
+            LEVEL 3
+        </button>
+            <svg class="line">
+            <line x1="580" y1="110" x2="740" y2="0" />
+            </svg>
+        <button class="boutonlevel1">
+            LEVEL 4
+        </button>
+            <svg class="line">
+            <line x1="920" y1="110" x2="740" y2="-30" />
+            </svg>
+        <button class="boutonlevel">
+            LEVEL 5
+        </button>
+        
     </div>
 
     <div class="box_btn">
+        <div class="onglet-titre">thème 2 : je mange des pâtes</div>
         <button class="boutonlevel">
             LEVEL 1
+        </button>
+            <svg class="line">
+            <line x1="130" y1="110" x2="240" y2="40" />
+            </svg>
+        <button class="boutonlevel1">
+            LEVEL 2
+        </button>
+            <svg class="line">
+            <line x1="480" y1="110" x2="240" y2="-60" />
+            </svg>
+        <button class="boutonlevel">
+            LEVEL 3
+        </button>
+            <svg class="line">
+            <line x1="580" y1="110" x2="740" y2="0" />
+            </svg>
+        <button class="boutonlevel1">
+            LEVEL 4
+        </button>
+            <svg class="line">
+            <line x1="920" y1="110" x2="740" y2="-30" />
+            </svg>
+        <button class="boutonlevel">
+            LEVEL 5
         </button>
     </div>
 
