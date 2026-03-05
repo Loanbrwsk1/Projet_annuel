f2eb8e19cc699c3b296af811e03af039b32157ca
Juvru <noar.0251@gmail.com>
2026-03-05 09:46:54 +0100

    footer/menu fixed

--- views/home.php
+++ views/home.php
@@ -12,88 +12,6 @@
 </head>
 
 <body>
-    <style>
-        .boutonlevel {
-            width: 155px;
-            height: 150px;
-            font-weight: bolder;
-            cursor: pointer;
-            color: #ffffff;
-            font-size: 17px;
-            border-radius: 50%;
-            border: none;
-            position: relative;
-            top : 80px;
-            background: inherit;
-            transition: 0.1s;
-            background: linear-gradient(90deg,
-                    rgba(0, 154, 214, 1) 0%,
-                    rgba(39, 64, 163, 1) 50%,
-                    rgba(210, 0, 247, 1) 100%);
-            background-attachment: fixed;
-            overflow: hidden;
-            z-index: 99;
-        }
-        .boutonlevel1 {
-            width: 155px;
-            height: 150px;
-            font-weight: bolder;
-            cursor: pointer;
-            color: #ffffff;
-            font-size: 17px;
-            border-radius: 50%;
-            border: none;
-            position: relative;
-            top: -80px;
-            background: inherit;
-            transition: 0.1s;
-            background: linear-gradient(90deg,
-                    rgba(0, 154, 214, 1) 0%,
-                    rgba(39, 64, 163, 1) 50%,
-                    rgba(210, 0, 247, 1) 100%);
-            background-attachment: fixed;
-            overflow: hidden;
-            z-index: 99;
-        }
-        .boutonlevel::after {
-            content: '';
-            width: 100%;
-            height: 100%;
-            filter: blur(15px);
-            z-index: -1;
-            position: absolute;
-            
-        }
-        .boutonlevel1::after {
-            content: '';
-            width: 100%;
-            height: 100%;
-            filter: blur(15px);
-            z-index: -1;
-            position: relative;
-            
-        }
-
-        .boutonlevel:active {
-            transform: scale(0.95);
-        }
-        .boutonlevel1:active {
-            transform: scale(0.95);
-        }
-
-        .box_btn {
-            background-color: #212121;
-            padding: 100px;
-            margin-top: 50px;
-            margin-left: 50px;
-            margin-right: 50px;
-            border-radius: 20px;
-            gap: 80px;
-
-        }
-        
-    </style>
-    
     <div class="top-bar">
         <div>
             <a href="index.php?action=logout">
@@ -118,29 +36,29 @@
         </button>
         <svg class="line">
             <line x1="130" y1="110" x2="240" y2="40" />
-            </svg>
+        </svg>
         <button class="boutonlevel1">
             LEVEL 2
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="480" y1="110" x2="240" y2="-60" />
-            </svg>
+        </svg>
         <button class="boutonlevel">
             LEVEL 3
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="580" y1="110" x2="740" y2="0" />
-            </svg>
+        </svg>
         <button class="boutonlevel1">
             LEVEL 4
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="920" y1="110" x2="740" y2="-30" />
-            </svg>
+        </svg>
         <button class="boutonlevel">
             LEVEL 5
         </button>
-        
+
     </div>
 
     <div class="box_btn">
@@ -148,30 +66,123 @@
         <button class="boutonlevel">
             LEVEL 1
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="130" y1="110" x2="240" y2="40" />
-            </svg>
+        </svg>
         <button class="boutonlevel1">
             LEVEL 2
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="480" y1="110" x2="240" y2="-60" />
-            </svg>
+        </svg>
         <button class="boutonlevel">
             LEVEL 3
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="580" y1="110" x2="740" y2="0" />
-            </svg>
+        </svg>
         <button class="boutonlevel1">
             LEVEL 4
         </button>
-            <svg class="line">
+        <svg class="line">
             <line x1="920" y1="110" x2="740" y2="-30" />
-            </svg>
+        </svg>
         <button class="boutonlevel">
             LEVEL 5
         </button>
+    </div>
+    <div class="box_btn">
+        <div class="onglet-titre">thème 3 : je mange des pâtes</div>
+        <button class="boutonlevel">
+            LEVEL 1
+        </button>
+        <svg class="line">
+            <line x1="130" y1="110" x2="240" y2="40" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 2
+        </button>
+        <svg class="line">
+            <line x1="480" y1="110" x2="240" y2="-60" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 3
+        </button>
+        <svg class="line">
+            <line x1="580" y1="110" x2="740" y2="0" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 4
+        </button>
+        <svg class="line">
+            <line x1="920" y1="110" x2="740" y2="-30" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 5
+        </button>
+
+    </div>
+    <div class="box_btn">
+        <div class="onglet-titre">thème 4 : je mange des pâtes</div>
+        <button class="boutonlevel">
+            LEVEL 1
+        </button>
+        <svg class="line">
+            <line x1="130" y1="110" x2="240" y2="40" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 2
+        </button>
+        <svg class="line">
+            <line x1="480" y1="110" x2="240" y2="-60" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 3
+        </button>
+        <svg class="line">
+            <line x1="580" y1="110" x2="740" y2="0" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 4
+        </button>
+        <svg class="line">
+            <line x1="920" y1="110" x2="740" y2="-30" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 5
+        </button>
+
+    </div>
+    <div class="box_btn">
+        <div class="onglet-titre">thème 5 : je mange des pâtes</div>
+        <button class="boutonlevel">
+            LEVEL 1
+        </button>
+        <svg class="line">
+            <line x1="130" y1="110" x2="240" y2="40" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 2
+        </button>
+        <svg class="line">
+            <line x1="480" y1="110" x2="240" y2="-60" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 3
+        </button>
+        <svg class="line">
+            <line x1="580" y1="110" x2="740" y2="0" />
+        </svg>
+        <button class="boutonlevel1">
+            LEVEL 4
+        </button>
+        <svg class="line">
+            <line x1="920" y1="110" x2="740" y2="-30" />
+        </svg>
+        <button class="boutonlevel">
+            LEVEL 5
+        </button>
+
     </div>
 
     <footer>
